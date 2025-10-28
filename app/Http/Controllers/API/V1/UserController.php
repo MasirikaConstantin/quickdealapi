420ef812e4d070c4c9d5ae4b81493b276d0c9e14
Masirika Constantin <elconstant73@gmail.com>
2025-10-28 22:55:41 +0100

    add build

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(User::first());
+    }
 }
